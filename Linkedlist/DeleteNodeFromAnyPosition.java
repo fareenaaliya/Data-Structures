664a2c3c2c493fd9c350567d3021202b354d665f
fareenaaliya <96537386+fareenaaliya@users.noreply.github.com>
2021-12-22 21:16:01 +0530

    Update DeleteNodeFromAnyPosition.java

--- Linkedlist/DeleteNodeFromAnyPosition.java
+++ Linkedlist/DeleteNodeFromAnyPosition.java
@@ -1,1 +1,21 @@
+#Delete a node from Singly Linked List at a given position
 
+public void DeleteAnyNode(int position)
+{
+    if(position == 1)
+    {
+        head=head.next;
+    }
+    else
+    {
+        ListNode previous = head;
+        int count = 1;
+        while(count<position-1)
+        {
+            previous = previous.next;
+            count++;
+        }
+    ListNode current = previous.next;
+    previous.next = current.next;
+    }
+}
